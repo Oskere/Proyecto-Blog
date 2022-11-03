7538300ecd2d75b3ca0bf96dd84a306c2dc8cd61
Oskere <91115094+Oskere@users.noreply.github.com>
2022-11-03 09:23:32 +0100

    commit 1
    añadir archivos a github

---
+++ c_mostrarPosts.php
@@ -0,0 +1,13 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "blog");
+if ($mysqli->connect_errno) {
+    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$resultado = $mysqli->query("SELECT * FROM `posts`");
+
+
+$mysqli->close();
+
+require ('v_mostrarPosts.php');
+?>
---
+++ v_mostarPosts.php
@@ -0,0 +1,30 @@
+<html>
+<head> 
+<style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        table {
+           margin:auto;
+            width: 30%;
+        }
+    </style>
+    </head>
+    <body>
+    <div style="width:100px; height:20px; border:1px solid black; text-align:center; margin:auto; margin-bottom:40px;">
+    <br><br>
+	</div>
+    <body>
+<html>
+<?php
+echo '<table>';
+while ($row = $resultado->fetch_assoc()) {
+    echo('<tr><td><a href="ver.php?DNI='.$row['DNI'].'">'.$row['Email'] .'</a></td><td>'. $row['Nombre'] .'</td><td><a style="text-decoration:none;" href="eliminar.php?DNI='.$row['DNI'].'">-</a></td></tr>');
+    
+}
+echo "</table>";
+?>
